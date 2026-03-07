ef8f48f95db241da7a8a4fa984ba86bcb782294a
RishabhMathur06 <rishabhmathur612@gmail.com>
2026-03-07 09:09:02 +0530

    Corrected .env path

--- src/config.py
+++ src/config.py
@@ -8,8 +8,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Loading environment variables.
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 class Config:
     # ----API Configuration----
